81d02ca45836ce270ebb2f3c464b28fbde9eed75
TonyInfinity <quachhengtony@gmail.com>
2021-05-22 14:59:56 +0700

    First commit

---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Post from "./Post/Post";
+
+import useStyles from "./styles";
+import { useSelector } from "react-redux";
+
+export default function Posts() {
+    const classes = useStyles();
+    const posts = useSelector(state => state.posts);
+
+    console.log(posts);
+
+    return (
+        <div>
+            Posts
+            <Post />
+        </div>
+    )
+}
